6abdff5dfa8ef35b91ae07fe90e8c35da3ee00a1
李小东 <825346670@qq.com>
2025-09-20 03:45:25 +0800

    Add files via upload

--- install_tools.sh
+++ install_tools.sh
@@ -1,2 +1,2 @@
 sudo wget -nv -O /root/android-ndk-r28c.zip "https://dl.google.com/android/repository/android-ndk-r28c-linux.zip"
-yes | sudo unzip /root/android-ndk-r28c.zip
+yes | sudo unzip /root/android-ndk-r28c.zip -d /root
